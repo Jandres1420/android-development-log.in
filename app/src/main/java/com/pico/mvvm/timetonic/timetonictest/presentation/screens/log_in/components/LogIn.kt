b1e70ac3bbfc439409081533685051530a9e2871
juan pico <japm1420@gmail.com>
2024-01-04 11:15:34 -0500

    Fixing the corroutine

--- app/src/main/java/com/pico/mvvm/timetonic/timetonictest/presentation/screens/log_in/components/LogIn.kt
+++ app/src/main/java/com/pico/mvvm/timetonic/timetonictest/presentation/screens/log_in/components/LogIn.kt
@@ -3,6 +3,10 @@ package com.pico.mvvm.timetonic.timetonictest.presentation.screens.log_in.compon
 import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -10,6 +14,7 @@ import com.pico.mvvm.timetonic.timetonictest.domain.model.Response
 import com.pico.mvvm.timetonic.timetonictest.presentation.components.ProgressBar
 import com.pico.mvvm.timetonic.timetonictest.presentation.navigation.AppScreen
 import com.pico.mvvm.timetonic.timetonictest.presentation.screens.log_in.viewModel.LogInViewModel
+
 /**
  *  Is the component that is responsabe for the Response of viewModel.logInResponse
  *  @param navController: NavHostController
@@ -18,6 +23,7 @@ import com.pico.mvvm.timetonic.timetonictest.presentation.screens.log_in.viewMod
 
 @Composable
 fun Login(navController: NavHostController, viewModel: LogInViewModel = hiltViewModel()) {
+    var createSessKeyCompleted by remember {mutableStateOf(false)}
     when (val logInResponse = viewModel.logInResponse) {
         Response.Loading -> {
             ProgressBar()
@@ -25,9 +31,14 @@ fun Login(navController: NavHostController, viewModel: LogInViewModel = hiltView
 
         is Response.Success -> {
             Toast.makeText(LocalContext.current, "Usuario Logeado", Toast.LENGTH_SHORT).show()
-            viewModel.createSessKey()
-            viewModel.clearState()
-            navController.navigate(route = AppScreen.Home.route)
+            LaunchedEffect(Unit) {
+                createSessKeyCompleted = viewModel.createSessKey()
+            }
+            if (createSessKeyCompleted) {
+                viewModel.clearState()
+                navController.navigate(route = AppScreen.Home.route)
+            }
+
         }
 
         is Response.Failure -> {
